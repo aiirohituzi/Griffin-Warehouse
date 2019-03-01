afcfa51c42908233e230b53a48fa87b72118b72a
aiirohituzi <sinn5656@gmail.com>
2019-03-01 22:42:51 +0900

    jQuery 설치, 상단 네비게이션 메뉴 기본틀 추가

--- Griffin-Warehouse/src/App.vue
+++ Griffin-Warehouse/src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <ul id="navigation">
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+    <div class="container-router-view">
+      <router-view/>
+    </div>
   </div>
 </template>
 
@@ -12,12 +18,35 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container-router-view {
+  margin-top: 5vh;
+}
+ul#navigation {
+    position: fixed;
+    margin: 0;
+    padding: 0;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 5vh;
+    list-style: none;
+    z-index:5;
+    
+    background-color:#c3b9a2;
+    -moz-box-shadow: -5px 2px 8px #555;
+    -webkit-box-shadow: -5px 2px 8px #555;
+    box-shadow: -5px 2px 8px #555;
+    
+	-ms-user-select: none;
+	-moz-user-select: -moz-none;
+	-khtml-user-select: none;
+	-webkit-user-select: none;
+	user-select: none;
+}
+ul#navigation li {
+  float: left;
+  width: calc(100% / 3);
+  height: 100%;
+	cursor: pointer;
 }
 </style>
--- Griffin-Warehouse/src/main.js
+++ Griffin-Warehouse/src/main.js
@@ -4,6 +4,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import 'expose-loader?$!expose-loader?jQuery!jquery'
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
