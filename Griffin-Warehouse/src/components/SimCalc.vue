5e7dfaec63e70ba14276e1f1b44f1b550164accb
aiirohituzi <sinn5656@gmail.com>
2019-03-16 22:14:30 +0900

    필요 모의작전점수 계산 참고용 자료로 각 레벨별 스킬칩 요구량 표 추가

--- Griffin-Warehouse/src/components/SimCalc.vue
+++ Griffin-Warehouse/src/components/SimCalc.vue
@@ -80,6 +80,72 @@
         </div>
         <div class="btn-getData" @click="openGetData()"></div>
     </div>
+
+    <div id="div-needData" class="side-right">
+        <div class="btn-needData" @click="openNeedData()"></div>
+        <table id="table-needData">
+            <tr>
+                <th>연구 레벨</th>
+                <th>인형</th>
+                <th>전략요정</th>
+                <th>전투요정</th>
+            </tr>
+            <tr>
+                <th>1 → 2</th>
+                <td class="bgcolor-basic">100</td>
+                <td class="bgcolor-basic">200</td>
+                <td class="bgcolor-basic">200</td>
+            </tr>
+            <tr>
+                <th>2 → 3</th>
+                <td class="bgcolor-basic">200</td>
+                <td class="bgcolor-basic">400</td>
+                <td class="bgcolor-basic">400</td>
+            </tr>
+            <tr>
+                <th>3 → 4</th>
+                <td class="bgcolor-basic">300</td>
+                <td class="bgcolor-basic">600</td>
+                <td class="bgcolor-basic">600</td>
+            </tr>
+            <tr>
+                <th>4 → 5</th>
+                <td class="bgcolor-intermediate">120</td>
+                <td class="bgcolor-intermediate">240</td>
+                <td class="bgcolor-intermediate">200</td>
+            </tr>
+            <tr>
+                <th>5 → 6</th>
+                <td class="bgcolor-intermediate">200</td>
+                <td class="bgcolor-intermediate">400</td>
+                <td class="bgcolor-intermediate">400</td>
+            </tr>
+            <tr>
+                <th>6 → 7</th>
+                <td class="bgcolor-intermediate">300</td>
+                <td class="bgcolor-intermediate">600</td>
+                <td class="bgcolor-intermediate">600</td>
+            </tr>
+            <tr>
+                <th>7 → 8</th>
+                <td class="bgcolor-intermediate">400</td>
+                <td class="bgcolor-intermediate">800</td>
+                <td class="bgcolor-advanced">200</td>
+            </tr>
+            <tr>
+                <th>8 → 9</th>
+                <td class="bgcolor-advanced">200</td>
+                <td class="bgcolor-advanced">400</td>
+                <td class="bgcolor-advanced">400</td>
+            </tr>
+            <tr>
+                <th>9 → 10</th>
+                <td class="bgcolor-advanced">300</td>
+                <td class="bgcolor-advanced">600</td>
+                <td class="bgcolor-advanced">600</td>
+            </tr>
+        </table>
+    </div>
 </div>
 </template>
 
@@ -136,7 +202,10 @@ export default {
         },
         openGetData: function () {
             $('.side-left').toggleClass('move')
-        }
+        },
+        openNeedData: function () {
+            $('.side-right').toggleClass('move')
+        },
     },
     updated: function () {
         this.simCalc()
@@ -228,5 +297,70 @@ div#div-getData .btn-getData {
 div#div-getData .btn-getData:hover {
     background: #c5c5c5;
     cursor: pointer;
+}
+
+.side-right {
+    -webkit-transform: translate(0, 0);
+    -webkit-transition: -webkit-transform 500ms;
+
+    transform: translate(0, 0);
+    transition: transform 500ms;
+
+    will-change: transform;
+}
+.side-right.move {
+    -webkit-transform: translate(-235px, 0px);
+    transform: translate(-235px, 0px);
+}
+div#div-needData {
+    position: fixed;
+    right: -235px;
+    top: 30vh;
+    width: 250px;
+    height: 220px;
+    border-top-left-radius: 5px;
+    border-bottom-left-radius: 5px;
+    background: #ddd;
+    -moz-box-shadow: 1px 3px 6px #999;
+    -webkit-box-shadow: 0px 3px 6px #999;
+    box-shadow: 1px 3px 6px #999;
+
+    text-align: center;
+}
+
+table#table-needData{
+    float: right;
+    font-size: 9pt;
+    margin: 10px;
+}
+table#table-needData td{
+    width: 50px;
+    padding: 0;
+}
+table#table-needData tr{
+    height: 10px;
+}
+div#div-needData .btn-needData {
+    width: 15px;
+    height: 100%;
+    line-height: 100%;
+    border-top-left-radius: 5px;
+    border-bottom-left-radius: 5px;
+    float: left;
+    background: #d3d3d3;
+}
+div#div-needData .btn-needData:hover {
+    background: #c5c5c5;
+    cursor: pointer;
+}
+
+.bgcolor-basic {
+    background-color: #449977;
+}
+.bgcolor-intermediate {
+    background-color: #5E8107;
+}
+.bgcolor-advanced {
+    background-color: #C8DA4B;
 }
 </style>
