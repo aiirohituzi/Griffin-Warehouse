cd7c2c6a30d2ae1c6cebe873933c2516b3f19d1b
aiirohituzi <sinn5656@gmail.com>
2019-03-30 21:17:30 +0900

    네비게이션바 CSS 수정 - 웹폰트 적용(구글 웹폰트) - 가로, 세로 중앙정렬 - 적절한 가변 폰트 크기 지정

--- Griffin-Warehouse/src/App.vue
+++ Griffin-Warehouse/src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
     <ul id="navigation">
-      <router-link tag="li" to="/damageCalc">damageCalc</router-link>
-      <router-link tag="li" to="/gemsCalc">gemsCalc</router-link>
-      <router-link tag="li" to="/simCalc">simCalc</router-link>
+      <router-link tag="li" to="/damageCalc">데미지 계산기</router-link>
+      <router-link tag="li" to="/gemsCalc">필요 보석량 계산기</router-link>
+      <router-link tag="li" to="/simCalc">필요 모의작전점수 계산기</router-link>
     </ul>
     <div class="container-router-view">
       <router-view/>
@@ -18,6 +18,8 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Do+Hyeon');
+
 .container-router-view {
   margin-top: 40px;
 }
@@ -47,9 +49,13 @@ ul#navigation li {
   box-sizing: border-box;
   float: left;
   width: calc(100% / 3);
+  line-height: 40px;
   height: 100%;
 	cursor: pointer;
-  /* border: 1px solid #000; */
+  
+  text-align: center;
+  font-size: 3vmin;
+  font-family: 'Do Hyeon', sans-serif;
 }
 ul#navigation li:hover {
     background-color:#9c9175;
