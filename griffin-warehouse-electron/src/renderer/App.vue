1de1087a07c149e8686f5e59b3a87c9856b0a34f
aiirohituzi <sinn5656@gmail.com>
2019-05-25 21:37:27 +0900

    issue #2: 기존의 App.vue 화면 띄우는데 성공함 문제 원인은 일단 electron + vue 환경에서 jquery를 import하는 방식이 다르다는것과 import에 성공하고 나서도 들여쓰기를 2칸 간격으로 철저히 유지해야 한다는 점, 연산자를 항상 띄어쓰기로 구분해야 인식한다는 점 등 자잘한 문제들이 원인

--- griffin-warehouse-electron/src/renderer/App.vue
+++ griffin-warehouse-electron/src/renderer/App.vue
@@ -1,15 +1,138 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <ul id="navigation">
+      <router-link tag="li" class="nav-home" to="/"></router-link>
+      <router-link tag="li" to="/damageCalc">데미지 계산기</router-link>
+      <router-link tag="li" to="/agiCalc">최대사속 계산기</router-link>
+      <router-link tag="li" to="/gemsCalc">필요 보석량 계산기</router-link>
+      <router-link tag="li" to="/simCalc">필요 모의작전점수 계산기</router-link>
+      <router-link tag="li" to="/expCalc">경험치 계산기</router-link>
+    </ul>
+    <div class="nav-tab">
+      <span style="width: 40px;"></span>
+      <span id="DamageCalc"></span>
+      <span id="AgiCalc"></span>
+      <span id="GemsCalc"></span>
+      <span id="SimCalc"></span>
+      <span id="ExpCalc"></span>
+    </div>
+    <div class="container-router-view">
+      <router-view/>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'griffin-electron'
+export default {
+  name: 'App',
+  methods: {
+    tabMove: function () {
+      // console.log(this.$route.name)
+      $('.nav-tab').children().removeClass()
+      $('#' + this.$route.name).addClass('current')
+    }
+  },
+  mounted: function () {
+    this.tabMove()
+  },
+  updated: function () {
+    this.tabMove()
   }
+}
 </script>
 
 <style>
-  /* CSS */
+@import url('https://fonts.googleapis.com/css?family=Do+Hyeon');
+
+.container-router-view {
+    margin-top: 40px;
+}
+ul#navigation {
+    position: fixed;
+    margin: 0;
+    padding: 0;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 40px;
+    list-style: none;
+    z-index:5;
+    
+    background-color:#c3b9a2;
+    /* -moz-box-shadow: 0px 2px 5px #555;
+    -webkit-box-shadow: 0px 2px 5px #555;
+    box-shadow: 0px 2px 5px #555; */
+    
+    -ms-user-select: none;
+    -moz-user-select: -moz-none;
+    -khtml-user-select: none;
+    -webkit-user-select: none;
+    user-select: none;
+}
+ul#navigation li {
+    box-sizing: border-box;
+    float: left;
+    width: calc((100% - 40px) / 5);
+    line-height: 40px;
+    height: 100%;
+    cursor: pointer;
+    
+    text-align: center;
+    font-size: 2vmin;
+    font-family: 'Do Hyeon', sans-serif;
+}
+ul#navigation li:hover {
+    background-color:#9c9175;
+}
+
+ul#navigation .nav-home {
+    box-sizing: border-box;
+    float: left;
+    width: 40px;
+    line-height: 40px;
+    height: 100%;
+    cursor: pointer;
+
+    background-repeat:no-repeat;
+    background-position:center center;
+    background-image: url(assets/home.png);
+}
+ul#navigation .nav-home:hover {
+    background-repeat:no-repeat;
+    background-position:center center;
+    background-image: url(assets/home_hover.png);
+}
+
+
+.nav-tab {
+    position: fixed;
+    margin: 0;
+    padding: 0;
+    top: 40px;
+    left: 0;
+    width: 100%;
+    height: 3px;
+    list-style: none;
+    z-index:4;
+    
+    background-color: #c3b9a2;
+    -moz-box-shadow: 0px 2px 5px #555;
+    -webkit-box-shadow: 0px 2px 5px #555;
+    box-shadow: 0px 2px 5px #555;
+    
+    -ms-user-select: none;
+    -moz-user-select: -moz-none;
+    -khtml-user-select: none;
+    -webkit-user-select: none;
+    user-select: none;
+}
+.nav-tab span{
+    box-sizing: border-box;
+    float: left;
+    width: calc((100% - 40px) / 5);
+    height: 100%;
+}
+.nav-tab .current{
+    background-color: #9c9175;
+}
 </style>
---
+++ griffin-warehouse-electron/src/renderer/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="container-home">
+    <h2>그리폰 물류창고</h2>
+
+    <p>소녀전선을 플레이하는데에 도움을 줄 수 있는 여러 툴을 사용해보실 수 있습니다.</p>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+    data () {
+        return {}
+    },
+}
+</script>
+
+<style>
+.container-home {
+    margin-top: calc(40px + 10vh);
+    margin-left: auto;
+    margin-right: auto;
+    width: 80vw;
+}
+</style>
