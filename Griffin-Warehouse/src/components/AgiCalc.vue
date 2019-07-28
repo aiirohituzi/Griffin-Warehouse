842fd747e967265a9a206a892ff8ecc6a70c13d2
aiirohituzi <sinn5656@gmail.com>
2019-07-28 18:49:21 +0900

    사속 계산기 스킬 선택 UI 변경 중 - 선택된 인형 병종에 따른 리스트를 보여주고 선택가능 하도록 UI 및 기능 수정

--- Griffin-Warehouse/src/components/AgiCalc.vue
+++ Griffin-Warehouse/src/components/AgiCalc.vue
@@ -26,7 +26,7 @@
             <div class="input-addon-left">스킬 선택</div>
             <div class="input-agi-allAddon-ex">
                 <select v-model="selectType">
-                    <option v-for="item in tdolltype" :value="item">{{ item }}</option>
+                    <option v-for="item in tdollType" :value="item">{{ item }}</option>
                 </select>
             </div>
             <div class="input-agi-allAddon">
@@ -292,52 +292,42 @@ export default {
     name: 'AgiCalc',
     data () {
         return {
-            tdolltype: ['AR', 'RF', 'SG'],
+            tdollType: ['AR', 'RF', 'SG'],
             selectType: 'AR',
-            skillList: [
-                {id: 0, name: '', percentage: 0},
-
-                {id: 1, name: '-------------------- AR --------------------', percentage: 0},
-                {id: 2, name: '고속사격T (ST AR-15 MOD, 97식)', percentage: 50},
-                {id: 3, name: '고속사격T (OTs-12)', percentage: 60},
-                
-                {id: 4, name: '강행돌파 (ART556, TAR-21)', percentage: 25},
-                {id: 5, name: '강행돌파 (마갈)', percentage: 20},
-                {id: 6, name: '강행돌파 (L85A1)', percentage: 15},
-                
-                {id: 7, name: '민접사격 (Model L)', percentage: 40},
-                
-                {id: 8, name: '백랑의 눈 (AK-12)', percentage: 35},
-                {id: 9, name: '여명의 기염 (CZ2000)', percentage: 50},
-                {id: 10, name: '미래 예지 (64식 소총)', percentage: 80},
-                
-                {id: 11, name: '설한의 쐐기 (M4A1 MOD2)', percentage: -70},
-                {id: 12, name: '아크 기어스 (G36 MOD2)', percentage: 20},
-
-
-                {id: 13, name: '-------------------- RF --------------------', percentage: 0},
-                {id: 14, name: '고속사격 (WA2000)', percentage: 75},
-                {id: 15, name: '고속사격 (SVD)', percentage: 65},
-                {id: 16, name: '비상사격 (K31)', percentage: 80},
-                {id: 17, name: '고속사격 (wz.29)', percentage: 60},
-                {id: 18, name: '고속사격 (시모노프, BM59)', percentage: 55},
-                {id: 19, name: '고속사격T (SM-1)', percentage: 40},
-                {id: 20, name: '고속사격N (XM3)', percentage: 100},
-                {id: 21, name: '고속사격N (81식 카빈)', percentage: 90},
-                {id: 22, name: '고속사격N (G43)', percentage: 85},
-
-                {id: 23, name: '민첩사격 (T-5000)', percentage: 50},
-                {id: 24, name: '민첩사격 (OBR)', percentage: 45},
-
-                {id: 25, name: '육참골단 (IWS 2000)', percentage: -35},
-                {id: 26, name: '분노의 발상 (Mk 12)', percentage: 30},
-                {id: 27, name: '싸늘한 투지 (FN-49 MOD2)', percentage: 15},
-                {id: 28, name: '데드라인 (4식)', percentage: 55},
-
-
-                {id: 29, name: '-------------------- SG --------------------', percentage: 0},
-                {id: 30, name: '광란의 파티 (USAS-12)', percentage: 60},
-                {id: 31, name: '금당현상 (AA-12)', percentage: 80},
+            skillList: [],
+            skillListAR: [
+                {id: 1, name: '고속사격T (ST AR-15 MOD, 97식)', percentage: 50},
+                {id: 2, name: '고속사격T (OTs-12)', percentage: 60},
+                {id: 3, name: '강행돌파 (ART556, TAR-21)', percentage: 25},
+                {id: 4, name: '강행돌파 (마갈)', percentage: 20},
+                {id: 5, name: '강행돌파 (L85A1)', percentage: 15},
+                {id: 6, name: '민접사격 (Model L)', percentage: 40},
+                {id: 7, name: '백랑의 눈 (AK-12)', percentage: 35},
+                {id: 8, name: '여명의 기염 (CZ2000)', percentage: 50},
+                {id: 9, name: '미래 예지 (64식 소총)', percentage: 80},
+                {id: 10, name: '설한의 쐐기 (M4A1 MOD2)', percentage: -70},
+                {id: 11, name: '아크 기어스 (G36 MOD2)', percentage: 20},
+            ],
+            skillListRF: [
+                {id: 1, name: '고속사격 (WA2000)', percentage: 75},
+                {id: 2, name: '고속사격 (SVD)', percentage: 65},
+                {id: 3, name: '비상사격 (K31)', percentage: 80},
+                {id: 4, name: '고속사격 (wz.29)', percentage: 60},
+                {id: 5, name: '고속사격 (시모노프, BM59)', percentage: 55},
+                {id: 6, name: '고속사격T (SM-1)', percentage: 40},
+                {id: 7, name: '고속사격N (XM3)', percentage: 100},
+                {id: 8, name: '고속사격N (81식 카빈)', percentage: 90},
+                {id: 9, name: '고속사격N (G43)', percentage: 85},
+                {id: 10, name: '민첩사격 (T-5000)', percentage: 50},
+                {id: 11, name: '민첩사격 (OBR)', percentage: 45},
+                {id: 12, name: '육참골단 (IWS 2000)', percentage: -35},
+                {id: 13, name: '분노의 발상 (Mk 12)', percentage: 30},
+                {id: 14, name: '싸늘한 투지 (FN-49 MOD2)', percentage: 15},
+                {id: 15, name: '데드라인 (4식)', percentage: 55},
+            ],
+            skillListSG: [
+                {id: 1, name: '광란의 파티 (USAS-12)', percentage: 60},
+                {id: 2, name: '금당현상 (AA-12)', percentage: 80},
             ],
 
             tdollAgi: '',
@@ -360,6 +350,19 @@ export default {
     },
     methods: {
         agiCalc: function () {
+            switch(this.selectType) {
+                case 'AR':
+                    this.skillList = this.skillListAR
+                    break
+                case 'RF':
+                    this.skillList = this.skillListRF
+                    break
+                case 'SG':
+                    this.skillList = this.skillListSG
+                    break
+                default:
+                    return
+            }
             // 현재 30fps 기준, 후에 테스트 서버의 60fps 기능이 도입되면 변경될 한계사속으로 수정 필요
             var sumAgiBuff = parseInt(this.agiBuff1) + parseInt(this.agiBuff2) + parseInt(this.agiBuff3) + parseInt(this.agiBuff4)
             var mulAgiSkill = (1 + this.agiSkill1 / 100) * (1 + this.agiSkill2 / 100) * (1 + this.agiSkill3 / 100) * (1 + this.agiSkill4 / 100)
