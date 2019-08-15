8e5768442faf8de428b0d929fc6a81dcf9e0d749
aiirohituzi <sinn5656@gmail.com>
2019-08-15 21:39:18 +0900

    - 인형제조시간 검색 용이하게 제조시간값 양식 변경 - 메인화면에 검색 테스트

--- Griffin-Warehouse/src/components/Home.vue
+++ Griffin-Warehouse/src/components/Home.vue
@@ -3,15 +3,40 @@
     <h2>그리폰 물류창고</h2>
 
     <p>소녀전선을 플레이하는데에 도움을 줄 수 있는 여러 툴을 사용해보실 수 있습니다.</p>
+
+
+    인형제조시간 검색
+    <input type="text" v-model="inputTime"><button @click="timeSearch()">검색</button>
+    {{ result }}
 </div>
 </template>
 
 <script>
+import { timetable } from './data.js'
 export default {
     name: 'Home',
     data () {
-        return {}
+        return {
+            timetable: [],
+            inputTime: '',
+            result: [],
+        }
     },
+    methods: {
+        timeSearch: function () {
+            this.result = []
+
+            for(var i=0; i<this.timetable.length; i++){
+                if(this.timetable[i].time == this.inputTime){
+                      this.result.push(this.timetable[i])
+                }
+            }
+            // console.log(this.result)
+        }
+    },
+    mounted: function () {
+        this.timetable = timetable
+    }
 }
 </script>
 
