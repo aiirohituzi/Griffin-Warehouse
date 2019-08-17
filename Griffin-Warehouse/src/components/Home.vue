0da9e058b6f0c4e0a977891740c3b9c45a41aebe
aiirohituzi <sinn5656@gmail.com>
2019-08-17 20:52:52 +0900

    제조시간 검색창 CSS 디자인

--- Griffin-Warehouse/src/components/Home.vue
+++ Griffin-Warehouse/src/components/Home.vue
@@ -5,9 +5,15 @@
     <p>소녀전선을 플레이하는데에 도움을 줄 수 있는 여러 툴을 사용해보실 수 있습니다.</p>
 
 
-    인형제조시간 검색
-    <input type="text" v-model="inputTime"><button @click="timeSearch()">검색</button>
-    {{ result }}
+
+    <div class="inputGroup-timetable">
+        <div class="input-addon-left">인형제조시간 검색</div>
+        <div class="input-timetable-allAddon">
+            <input type="text" maxlength="4" v-model="inputTime">
+        </div>
+        <div class="input-addon-right" @click="timeSearch()">검색</div>
+    </div>
+    <div class="result-timetable">{{ result }}</div>
 </div>
 </template>
 
@@ -46,5 +52,82 @@ export default {
     margin-left: auto;
     margin-right: auto;
     width: 80vw;
+}
+.inputGroup-timetable {
+    width: fit-content;
+    height: 30px;
+    margin-top: 10vh;
+    margin-bottom: 1vh;
+    margin-left: auto;
+    margin-right: auto;
+}
+.inputGroup-timetable .input-addon-left {
+    float: left;
+    width: fit-content;
+    border: 2px solid #c3b9a2;
+    border-top-left-radius: 5px;
+    border-bottom-left-radius: 5px;
+    border-right: unset;
+    background-color: #d4cdbd;
+    padding: 3px;
+    font-size: 10pt;
+}
+.inputGroup-timetable .input-timetable-allAddon {
+    float: left;
+    border: 2px solid #c3b9a2;
+    -webkit-appearance: none;
+
+    border-radius: 0;
+    -webkit-border-radius: 0;
+
+    padding: 3px;
+
+    font-size: 10pt;
+    width: 50px;
+}
+.inputGroup-timetable .input-timetable-allAddon input{
+    -webkit-appearance: none;
+
+    border: unset;
+    -webkit-border: unset;
+    
+    margin: 0;
+    -webkit-margin: 0;
+
+    padding: 0;
+    -webkit-padding: 0;
+
+    font-size: 10pt;
+    width: 50px;
+    text-align: right;
+}
+.inputGroup-timetable .input-addon-right {
+    float: left;
+    width: fit-content;
+    border: 2px solid #c3b9a2;
+    border-top-right-radius: 5px;
+    border-bottom-right-radius: 5px;
+    border-left: unset;
+    background-color: #d4cdbd;
+    background: linear-gradient(to bottom, #f3f0e9 40%, #d4cdbd 100%);
+    padding: 3px;
+    font-size: 10pt;
+    
+    cursor: pointer;
+    -ms-user-select: none;
+    -moz-user-select: -moz-none;
+    -khtml-user-select: none;
+    -webkit-user-select: none;
+    user-select: none;
+}
+.inputGroup-timetable .input-addon-right:hover {
+    background-color: #9c9175;
+    background: linear-gradient(to bottom, #d4cdbd 40%, #9c9175 100%);
+}
+
+.result-timetable {
+    width: 40vw;
+    margin-left: auto;
+    margin-right: auto;
 }
 </style>
